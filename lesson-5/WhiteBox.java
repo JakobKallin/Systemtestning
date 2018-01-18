1acb4e27fa4688e049a5754f0b46ab8918387f13
Jakob Kallin <newton@jakobkallin.com>
2018-01-18 10:47:31 +0100

    Lesson 5

---
+++ lesson-5/WhiteBox.java
@@ -0,0 +1,13 @@
+public class WhiteBox {
+    public static int russianMultiplication(int x, int y) {
+        int result = 0;
+        while (x != 0) {
+            if (x % 2 != 0) {
+                result = result + y ;
+            }
+            x = x / 2;
+            y = y * 2;
+        }
+        return result;
+    }
+}
